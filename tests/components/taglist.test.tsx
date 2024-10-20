99bb65835101c2d6cb46c4fb97d9ffc03988b095
rasouljabbari <rjdeveloper17@gmail.com>
2024-10-20 22:00:28 +0330

    change wait to find

--- tests/components/taglist.test.tsx
+++ tests/components/taglist.test.tsx
@@ -2,12 +2,15 @@ import { render, screen, waitFor } from '@testing-library/react'
 import TagList from '../../src/components/TagList'
 
 describe('TagList', () => {
-    it('should render tag list',async () => {
+    it('should render tag list', async () => {
         render(<TagList />)
 
-        await waitFor(() => {
-            const listItems = screen.getAllByRole('listitem')
-            expect(listItems.length).toBeGreaterThan(0)
-        })
+        // await waitFor(() => {
+        //     const listItems = screen.getAllByRole('listitem')
+        //     expect(listItems.length).toBeGreaterThan(0)
+        // })
+
+        const listItems = await screen.findAllByRole('listitem')
+        expect(listItems.length).toBeGreaterThan(0)
     })
 })
