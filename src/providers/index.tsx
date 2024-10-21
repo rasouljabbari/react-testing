af5a26304bb3f57ffd52c1e862b3c80008ecb82e
rasouljabbari <rjdeveloper17@gmail.com>
2024-10-21 10:29:26 +0330

    add react-query components

---
+++ src/providers/index.tsx
@@ -0,0 +1,20 @@
+import { Theme } from "@radix-ui/themes";
+import "@radix-ui/themes/styles.css";
+import { PropsWithChildren } from "react";
+import ReactQueryProvider from "./ReactQueryProvider";
+
+const Providers = ({ children }: PropsWithChildren) => {
+  return (
+    // <AuthProvider>
+      <ReactQueryProvider>
+          {/* <CartProvider> */}
+            {/* <LanguageProvider language="en"> */}
+              <Theme>{children}</Theme>
+            {/* </LanguageProvider> */}
+          {/* </CartProvider> */}
+      </ReactQueryProvider>
+    // </AuthProvider>
+  );
+};
+
+export default Providers;
